dd606c595ced63d1f088879c2c771176c8f9eab0
VictorKrause <vkraause@hotmail.com>
2017-03-21 20:32:54 -0300

    Testes Unitários da classe Caixa Implementados
    Feito ajuste na caixa Teste: Adicionado um Try Catch na hora de buscar o
    cliente.
    Quebrava caso a fila estivesse vazia.

--- ReaquecimentoBanco/src/data/Caixa.java
+++ ReaquecimentoBanco/src/data/Caixa.java
@@ -2,38 +2,44 @@ package data;
 import java.util.*;
 public class Caixa {
 	private boolean prioritario;
-	
-	
+
+
 	public Caixa(boolean prioritario){
 		this.prioritario = prioritario;
 	}
-	
+
 	public String getCli (ArrayList <Cliente> filaPrioritaria, ArrayList<Cliente>filaNormal){
-		String nomeProxCliente = null;
-		Cliente cliRemover = null;
-		
-		if(!filaPrioritaria.isEmpty() && prioritario){
-			Cliente proxCli =filaPrioritaria.get(0);
-			nomeProxCliente = proxCli.getNome();
-			filaPrioritaria.remove(proxCli);
 
-			for(Cliente cli: filaNormal){
-				String nome = cli.getNome();
-				if(nomeProxCliente.equals(nome))
-					cliRemover = cli;
+		try{
+			String nomeProxCliente = null;
+			Cliente cliRemover = null;
+
+			if(!filaPrioritaria.isEmpty() && prioritario){
+				Cliente proxCli =filaPrioritaria.get(0);
+				nomeProxCliente = proxCli.getNome();
+				filaPrioritaria.remove(proxCli);
+
+				for(Cliente cli: filaNormal){
+					String nome = cli.getNome();
+					if(nomeProxCliente.equals(nome))
+						cliRemover = cli;
+				}
+
+				filaNormal.remove(cliRemover);
+				return nomeProxCliente;
 			}
-			
-			filaNormal.remove(cliRemover);
-			return nomeProxCliente;
-		}
-		
-		nomeProxCliente = filaNormal.get(0).getNome();
-		filaNormal.remove(0);
-		
-		return nomeProxCliente;
 
+			nomeProxCliente = filaNormal.get(0).getNome();
+			filaNormal.remove(0);
+
+			return nomeProxCliente;
+
+		}
+		catch (Exception e){
+			return null;
+		}
 	}
 }
-	
-	
+
+
 
--- ReaquecimentoBanco/src/testes/CaixaTester.java
+++ ReaquecimentoBanco/src/testes/CaixaTester.java
@@ -2,13 +2,97 @@ package testes;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
+import data.*;
 
 public class CaixaTester {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	ArrayList<Cliente> filaPri = new ArrayList<Cliente>();
+	ArrayList<Cliente> filaNormal = new ArrayList<Cliente>();
+
+	Cliente idoso;
+	Cliente jovem;
+
+	Caixa caixaPri;
+	Caixa caixaNormal;
+
+	@Before
+	public void setUp(){
+		// Criar um Caixa Prioritário e um Normal
+		caixaPri = new Caixa(true);
+		caixaNormal = new Caixa(false);
+
+		// Criar filas prioritária e normal
+		filaPri = new ArrayList<Cliente>();
+		filaNormal = new ArrayList<>();
+
+		//Criar  os clientes
+		idoso = new Cliente("Idoso", 65);
+		jovem = new Cliente("Jovem", 15);
+
+		//Resetar as Filas
+		filaPri.clear();
+		filaNormal.clear();
+
+		//Adicionmar os Clientes nas filas
+		filaNormal.add(jovem);
+		filaPri.add(idoso);
+		filaNormal.add(idoso);
+
 	}
 
+	@Test
+	public void testCaixaPrioritarioRetornarIdoso (){
+		String  expected = caixaPri.getCli(filaPri, filaNormal);
+
+		assertEquals(expected, idoso.getNome());	
+	}
+
+	@Test
+	public void testCaixaNormalRetornarJovem(){
+		String expected = caixaNormal.getCli(filaPri, filaNormal);
+
+		assertEquals(expected, jovem.getNome());
+	}
+
+	@Test
+	public void testFilaPrioritariaVazia(){	
+		filaPri.clear();
+		filaNormal.clear();
+		assertNull(caixaPri.getCli(filaPri, filaNormal));	
+	}
+
+	@Test
+	public void testFilaNormalVazia(){
+		filaNormal.clear();
+		assertNull(caixaNormal.getCli(filaPri, filaNormal));
+	}
+
+	@Test
+	public void testRemoverIdosoAposChamada(){
+		caixaPri.getCli(filaPri, filaNormal);
+		boolean contem = false;
+
+		if((filaPri.contains(idoso)||filaNormal.contains(idoso)))
+			contem = true;
+
+		assertFalse(contem);
+	}
+	
+	@Test
+	public void testRemoverJovemAposChamada(){
+		boolean contem = false;
+		
+		caixaNormal.getCli(filaPri, filaNormal);
+		
+		if(filaNormal.contains(jovem))
+			contem = true;
+		
+		assertFalse(contem);
+	}
 }
+
+
